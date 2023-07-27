7da1622092cc8b1b4f965fbfca3a6e760bc0c5b4
Niwis (Nicolas) <112716299+NiwiSB@users.noreply.github.com>
2023-07-27 16:42:04 +0200

    Init Laravel + Config LAV

---
+++ html/config/website.php
@@ -0,0 +1,27 @@
+<?php
+
+// Obligatoire de remplir les indications sans instructions
+
+return[
+    'nom_du_site_maison' => env('APP_NAME', 'OnPlots'),
+    'adresse_de_la_maison_logosse' => '/img/logosse.png',
+    'adresse_de_la_maison_iconesse' => '/img/iconesse.png',
+
+    'mot_de_passe_de_maintenance' => [
+        'mv4PJDo6mRK0WTNkz3uEx3qlrWlOI2DhqPn0WIR7wW7i8JLQhs'
+    ],
+
+    'adresses_sociaux' => [
+        'discoressed' => 'https://socials.onplots.com/gg',
+        'x' => 'https://socials.onplots.com/x'
+    ],
+
+    'utilisation_dun_systeme_de_rendu_simplifie_en_2d' => 1, // Optionnel
+    'utilisation_dun_systeme_de_rendu_avance_en_3d' => 1, // Optionnel
+
+
+    // Optionnel
+    'configuration_du_systeme_de_rendu' => [
+        'affichage_3x' => '',
+    ]
+];
